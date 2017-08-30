066e7f077d6fe471007dfdfcbbbf1d7c5e32c206
lorvis <remenocor@gmail.com>
2017-08-30 17:41:06 +0300

    First try

---
+++ YiiTest/home/site-form/www/protected/views/mail/basic.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+    Dear applicant, there's information you sent in application to Default Co.
+
+    Name: <?php echo $ApplicData->name ?>
+    Phone Number: <?php echo $ApplicData->phone_number ?>
+    Email: <?php echo $ApplicData->email ?>
+
+    Sincerely, director of Default Co.
+    </body>
+</html>
---
+++ YiiTest/home/site-form/www/protected/views/site/index.php
@@ -0,0 +1,7 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<a href="https://site-form/index.php/application/create"><h2>Create your application</h2></a>
